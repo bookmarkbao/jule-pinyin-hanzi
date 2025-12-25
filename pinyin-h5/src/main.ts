063d998c65b52437f052840f06cefd8290ed4dd4
deepdanci <61926298+bookmarkbao@users.noreply.github.com>
2025-12-25 12:34:28 +0800

    feat: add voice from cnchar

--- pinyin-h5/src/main.ts
+++ pinyin-h5/src/main.ts
@@ -3,7 +3,6 @@ import './style.css'
 // 2. Import the components style
 import 'vant/lib/index.css';
 import App from './App.vue'
-import { initVConsole } from '@/lib/vconsole'
-
-void initVConsole()
+// import { initVConsole } from '@/lib/vconsole'
+// void initVConsole()
 createApp(App).mount('#app')
